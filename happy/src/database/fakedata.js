599c5706ca6f4bebd7aa630f33e61ca9ced22cd1
Breno Norberto <brenonorberto@gmail.com>
2020-11-19 23:08:30 -0300

    Atualizando projeto happy

---
+++ happy/src/database/fakedata.js
@@ -0,0 +1,38 @@
+module.exports [
+    {
+        id: 1,
+        lat: "-3.7987653",
+        lng: "-38.4994594",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças 6 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1573261524391-266433971be1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1598322313658-512b47702270?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas das 08h até 18h",
+        open_on_weekends: "1"
+
+
+
+    },
+
+    {
+        id: 2,
+        lat: "-3.7987653",
+        lng: "-38.5094594",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças 6 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1573261524391-266433971be1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1598322313658-512b47702270?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas das 08h até 18h",
+        open_on_weekends: "0"
+
+    }
+
+]
--- happy/src/server.js
+++ happy/src/server.js
@@ -1,17 +1,24 @@
 // Importar dependencia
 const express = require('express');
 const path = require('path');
+const pages = require('./pages.js'); 
 
 // iniciando express
 const server = express();
 server
+
 // utilizando arquivos estaticos
 .use(express.static('public'))
 
-// Criando rota
-.get('/', (request, response) => {
-    return response.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
+// Configurar template engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
+// rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
 
 // Ligar Servidor
 server.listen(5500) 
