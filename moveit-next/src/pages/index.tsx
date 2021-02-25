aaf3409d11b9eeac11be392d0ff6d58e33107e98
Breno Norberto <brenonorberto@gmail.com>
2021-02-24 22:17:26 -0300

    Atualizaçoes Aula 2

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,10 +1,30 @@
+import React from "react";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { CountDown } from "../components/CountDown";
 import { ExperienceBar } from "../components/ExperienceBar";
-import Head from 'next/head'
+import { Profile } from "../components/Profile";
+import styles from "../styles/pages/Home.module.css";
+import Head from "next/head";
+
 
 export default function Home() {
   return (
-    <div className="container">
-      <ExperienceBar />     
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <CountDown />
+        </div>
+        <div>
+
+        </div>
+      </section>     
     </div>
   )
 }
